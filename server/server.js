6b261da128823ed6405acd73ccc912e3f07efea0
chandramouleeswaran77 <chandramouleeswaran770@gmail.com>
2026-09-18 22:18:16 +0530

    fix: use /{*path} wildcard for Express 5 compatibility

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use('/api/analytics', require('./routes/analytics'));
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
